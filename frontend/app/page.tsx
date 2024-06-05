4977dcc978f0e9fa3a898205bf5a3f37083275af
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-06-06 00:16:40 +0500

    add navbar menu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hi developer
-      <Button variant="destructive">Button</Button>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function Navbar() {
+  return (
+    <header className="text-gray-600 body-font bg-slate-500">
+      <div className="container mx-auto flex flex-wrap justify-between p-5 flex-col md:flex-row items-center">
+        <p className="ml-3 text-lg flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          Dashboard
+        </p>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
