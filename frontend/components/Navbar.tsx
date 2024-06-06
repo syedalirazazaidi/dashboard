1fca7cba4351b8b588536702d6f23a76b40cb094
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-06-07 00:21:15 +0500

    add dashboard

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -8,14 +10,21 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="text-gray-600 body-font bg-slate-500">
-      <div className="container mx-auto flex flex-wrap justify-between p-5 flex-col md:flex-row items-center">
-        <p className="ml-3 text-lg flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          Dashboard
-        </p>
+    <header className="text-gray-600 body-font bg-slate-300">
+      <div className="container mx-auto flex flex-wrap justify-between p-2 flex-col md:flex-row items-center">
+        <div>
+          <Link
+            className='className="ml-3 text-lg flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"'
+            href="/"
+          >
+            Dashboard
+          </Link>
+        </div>
+
         <DropdownMenu>
           <DropdownMenuTrigger className="focus:outline-none">
             <Avatar>
@@ -26,8 +35,13 @@ export default function Navbar() {
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              {" "}
+              <Link href="/logout">Logout</Link>
+            </DropdownMenuItem>
             <DropdownMenuItem>Team</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
