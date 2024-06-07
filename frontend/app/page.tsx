5e9b5e4ff7098e720d7a9b6ab8c3fd666cde5d3f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-06-07 21:13:48 +0500

    do header responssive

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { Dashboard } from "../components/board";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Dashboard />
+    // </main>
   );
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -10,42 +12,172 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
 
 export default function Navbar() {
   return (
-    <header className="text-gray-600 body-font bg-slate-300">
-      <div className="container mx-auto flex flex-wrap justify-between p-2 flex-col md:flex-row items-center">
-        <div>
-          <Link
-            className='className="ml-3 text-lg flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"'
-            href="/"
-          >
-            Dashboard
-          </Link>
+    <div className="flex flex-col">
+      <header className=" flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="flex flex-col">
+            <nav className="grid gap-2 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Package2 className="h-6 w-6" />
+                <span className="sr-only">Acme Inc</span>
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Home className="h-5 w-5" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+              >
+                <ShoppingCart className="h-5 w-5" />
+                Orders
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Package className="h-5 w-5" />
+                Products
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Users className="h-5 w-5" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <LineChart className="h-5 w-5" />
+                Analytics
+              </Link>
+            </nav>
+            <div className="mt-auto">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Upgrade to Pro</CardTitle>
+                  <CardDescription>
+                    Unlock all features and get unlimited access to our support
+                    team.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Button size="sm" className="w-full">
+                    Upgrade
+                  </Button>
+                </CardContent>
+              </Card>
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="w-full flex-1">
+          <form className="sm:ml-28   md:ml-52 lg:ml-64 ">
+            <div className="relative ">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search products..."
+                className=" w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              />
+            </div>
+          </form>
         </div>
-
         <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent align="end">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link href="/profile">Profile</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              {" "}
-              <Link href="/logout">Logout</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      </div>
-    </header>
+      </header>
+    </div>
+    // <header className="text-gray-600 body-font bg-slate-300">
+    //   <div className="container mx-auto flex flex-wrap justify-between p-2 flex-col md:flex-row items-center">
+    //     <div>
+    //       <Link
+    //         className='className="ml-3 text-lg flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"'
+    //         href="/"
+    //       >
+    //         Dashboard
+    //       </Link>
+    //     </div>
+
+    //     <DropdownMenu>
+    //       <DropdownMenuTrigger className="focus:outline-none">
+    //         <Avatar>
+    //           <AvatarImage src="https://github.com/shadcn.png" />
+    //           <AvatarFallback>CN</AvatarFallback>
+    //         </Avatar>
+    //       </DropdownMenuTrigger>
+    //       <DropdownMenuContent>
+    //         <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    //         <DropdownMenuSeparator />
+    //         <DropdownMenuItem>
+    //           <Link href="/profile">Profile</Link>
+    //         </DropdownMenuItem>
+    //         <DropdownMenuItem>
+    //           {" "}
+    //           <Link href="/logout">Logout</Link>
+    //         </DropdownMenuItem>
+    //         <DropdownMenuItem>Team</DropdownMenuItem>
+    //       </DropdownMenuContent>
+    //     </DropdownMenu>
+    //   </div>
+    // </header>
   );
 }
