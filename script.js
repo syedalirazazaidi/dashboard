e143cb99673140aa5aee45f42f8331e42b468bfb
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-06-20 23:26:01 +0500

    sort an array

--- script.js
+++ script.js
@@ -1,101 +1,120 @@
-console.log("hi developer");
-// reverser the string
-// var str = "aliraza";
-// var reverse = "";
-// for (let item of str) {
-//   reverse = item + reverse;
-// }
-// var left = 0;
-// var right = str.length - 1;
-// while (reverse <= right) {
+// console.log("hi developer");
+// // reverser the string
+// // var str = "aliraza";
+// // var reverse = "";
+// // for (let item of str) {
+// //   reverse = item + reverse;
+// // }
+// // var left = 0;
+// // var right = str.length - 1;
+// // while (reverse <= right) {
 
-// }
-// console.log(reverse);
+// // }
+// // console.log(reverse);
 
-// find number of occurance in inside the string
-// var mystr = "mynameisalirazazaidi";
-// var hashmap = {};
-// for (const char of mystr) {
-//   if (hashmap[char]) {
-//     hashmap[char] = hashmap[char] + 1;
-//   } else {
-//     hashmap[char] = 1;
-//   }
-// }
-// console.log(hashmap);
+// // find number of occurance in inside the string
+// // var mystr = "mynameisalirazazaidi";
+// // var hashmap = {};
+// // for (const char of mystr) {
+// //   if (hashmap[char]) {
+// //     hashmap[char] = hashmap[char] + 1;
+// //   } else {
+// //     hashmap[char] = 1;
+// //   }
+// // }
+// // console.log(hashmap);
 
-// count all the character
+// // count all the character
 
-// var mycha = "tttthisisrazazaidi";
-// var newmap = {};
-// for (const char of mycha) {
-//   if (newmap[char]) {
-//     newmap[char] = newmap[char] + 1;
-//   } else {
-//     newmap[char] = 1;
-//   }
-// }
-// console.log(newmap);
+// // var mycha = "tttthisisrazazaidi";
+// // var newmap = {};
+// // for (const char of mycha) {
+// //   if (newmap[char]) {
+// //     newmap[char] = newmap[char] + 1;
+// //   } else {
+// //     newmap[char] = 1;
+// //   }
+// // }
+// // console.log(newmap);
 
-// sum of array
-// var sum = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
-// var plus = 0;
+// // sum of array
+// // var sum = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+// // var plus = 0;
 
-// for (let index = 0; index < sum.length; index++) {
-//   const element = sum[index];
-//   plus = plus + element;
-// }
-// console.log(plus);
+// // for (let index = 0; index < sum.length; index++) {
+// //   const element = sum[index];
+// //   plus = plus + element;
+// // }
+// // console.log(plus);
 
-// sum of pair whose sum is zero
-// function sumaaryy() {
-//   var arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
-//   for (let index = 0; index < arr.length; index++) {
-//     for (let j = 0; j < arr.length; j++) {
-//         var sum = arr[index]+arr[j]
-//         if(sum===0){
-//     //   return{ value:[arr[left], arr[right]],index:[left,right]}
-// return [index,j]
-//         }
+// // sum of pair whose sum is zero
+// // function sumaaryy() {
+// //   var arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+// //   for (let index = 0; index < arr.length; index++) {
+// //     for (let j = 0; j < arr.length; j++) {
+// //         var sum = arr[index]+arr[j]
+// //         if(sum===0){
+// //     //   return{ value:[arr[left], arr[right]],index:[left,right]}
+// // return [index,j]
+// //         }
+// //     }
+
+// //   }
+// //   var left = 0;
+// //   var right = arr.length - 1;
+// //   while (left < right) {
+// //     var sum = arr[left] + arr[right];
+// //     if (sum === 0) {
+// //       return{ value:[arr[left], arr[right]],index:[left,right]}
+// //     }
+// //     left++;
+// //     right--;
+// //   }
+// // }
+
+// // var newarray=sumaaryy()
+// // console.log(newarray);
+
+// //check anagram // sort an array and count alphabet
+
+// // string anagram
+// // hello --> olleo
+// // raza-->zara
+// function anagram(str1, str2) {
+//   var maphash = {};
+//   //   for (const value of str1) {
+//   //     maphash[value] = (maphash[value] ?? 0) + 1;
+//   //   }
+//   //   return maphash
+//   for (const value of str1) {
+//     if (maphash[value]) {
+//       maphash[value] = maphash[value] + 1;
+//     } else {
+//       maphash[value] = 1;
 //     }
-
 //   }
-//   var left = 0;
-//   var right = arr.length - 1;
-//   while (left < right) {
-//     var sum = arr[left] + arr[right];
-//     if (sum === 0) {
-//       return{ value:[arr[left], arr[right]],index:[left,right]}
-//     }
-//     left++;
-//     right--;
-//   }
+//   return maphash
+// }
+// var object = anagram("alii", "ila");
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
 // }
 
-// var newarray=sumaaryy()
-// console.log(newarray);
+// sort an array
 
-//check anagram // sort an array and count alphabet
+var sortarar = [4, 9, 1, 10, 11, 0, 10];
 
-// string anagram
-// hello --> olleo
-// raza-->zara
-function anagram(str1, str2) {
-  var maphash = {};
-  //   for (const value of str1) {
-  //     maphash[value] = (maphash[value] ?? 0) + 1;
-  //   }
-  //   return maphash
-  for (const value of str1) {
-    if (maphash[value]) {
-      maphash[value] = maphash[value] + 1;
-    } else {
-      maphash[value] = 1;
+function SortArray(params) {
+  for (let value of params) {
+    for (let index = 0; index < params.length; index++) {
+      var element = params[index];
+      if (value > element) {
+        [value, element] = [element, value];
+      }
     }
   }
-  return maphash
+  console.log(params);
 }
-var object = anagram("alii", "ila");
-for (const property in object) {
-  console.log(`${property}: ${object[property]}`);
-}
+
+var newvalu = SortArray(sortarar);
+console.log(newvalu);
